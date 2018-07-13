c9ff107b7851fc05654bd4fb8c71461e75e030e3
Daisuke Takahashi <Daisuke.Takahashi@jp.sony.com>
2018-07-14 06:41:12 +0900

    running on docker

--- lib/audio.py
+++ lib/audio.py
@@ -5,7 +5,8 @@ import sys
 from os import path
 DIR = path.realpath(path.join(path.dirname(path.realpath(__file__)), '../audios'))
 
-def generate_wav(text):
+def generate_wav(text, mechpath='/var/lib/mecab/dic/open-jtalk/naist-jdic', htsvoicepath='/usr/share/hts-voice/mei/mei_happy.htsvoice'):
+
     fname = md5(text.encode('utf-8')).hexdigest()
     fpath = path.join(DIR, fname + '.wav')
     u"""
@@ -14,8 +15,8 @@ def generate_wav(text):
     via http://qiita.com/kkoba84/items/b828229c374a249965a9.
     """
     open_jtalk = ['open_jtalk']
-    mech = ['-x', '/usr/local/Cellar/open-jtalk/1.10_1/dic']
-    htsvoice = ['-m', '/usr/local/Cellar/open-jtalk/1.10_1/voice/mei/mei_happy.htsvoice']
+    mech = ['-x', mechpath]
+    htsvoice = ['-m',htsvoicepath] 
     # 再生速度
     speed = ['-r', '1.3']
     outwav = ['-ow', fpath]
@@ -28,6 +29,8 @@ def generate_wav(text):
 
 if __name__ == '__main__':
     text = sys.argv[1]
-    fpath = generate_wav(text)
+    mechpath = '/usr/local/Cellar/open-jtalk/1.10_1/dic'
+    htsvoicepath = '/usr/local/Cellar/open-jtalk/1.10_1/voice/mei/mei_happy.htsvoice'
+    fpath = generate_wav(text, mechpath, htsvoicepath)
     #  aplay = ['afplay', '-q', fpath+'.wav']
     #  wr = subprocess.Popen(aplay)
